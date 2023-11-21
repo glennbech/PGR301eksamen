9920449630b5083eb2ac9df2644af5c95ea762eb
RobinFlaks <robin_flaks@hotmail.com>
2023-11-21 09:47:54 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ resource "aws_apprunner_service" "service" {
   service_name = var.instance_name
 
   instance_configuration {
-    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn,
+    instance_role_arn = aws_iam_role.role_for_apprunner_service.arn
     "Cpu": "0.25 vCPU",
     "Memory": "1 GB"
   }
